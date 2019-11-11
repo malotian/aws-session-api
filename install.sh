5aa7b0e7a7492080b7fea1dda1c8506ebc109a26
root <malotian@gmail.com>
2019-11-11 22:48:29 +0000

    install

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-zip package.zip src pom.xml build.sh
-pkgname=`fission pkg create --sourcearchive package.zip --env nodeenv | cut -f2 -d "'"`
+name=`basename $(pwd)`
+zip -r ${name}.zip src pom.xml build.sh
+fission env create --name java --image fission/jvm-env --builder fission/jvm-builder --keeparchive --version 2
+pkgname=`fission pkg create --sourcearchive ${name}.zip --env java | cut -f2 -d "'"`
 echo $pkgname
-fission fn delete --name aws.session.api
-fission fn create --name aws.session.api --pkg $pkgname --entrypoint com.lingk.aws.session.api.AWSSessionTokenHelper
+fission fn delete --name ${name}
+fission fn create --name ${name} --pkg $pkgname --entrypoint com.lingk.aws.session.api.AWSSessionTokenHelper --createingress
+
+
