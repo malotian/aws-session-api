aebd0b7fbbef12669424dc806bb7603caa5379e3
root <malotian@gmail.com>
2019-11-11 23:05:22 +0000

    install-fix

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+set -x #echo on
 name=`basename $(pwd)`
 zip -r ${name}.zip src pom.xml build.sh
 fission env create --name java --image fission/jvm-env --builder fission/jvm-builder --keeparchive --version 2
 pkgname=`fission pkg create --sourcearchive ${name}.zip --env java | cut -f2 -d "'"`
 echo $pkgname
 fission fn delete --name ${name}
-fission fn create --name ${name} --pkg $pkgname --entrypoint com.lingk.aws.session.api.AWSSessionTokenHelper --createingress
+fission fn create --name ${name} --pkg $pkgname --entrypoint com.lingk.aws.session.api.AWSSessionTokenHelper
+fission route create --url /${name} --function ${name} --createingress
+
 
 
