27bda1f21512b4ad4457e45387d1acdde6163d78
Harjinder Dhanjal <malotian@gmail.com>
2019-11-11 20:12:26 -0500

    help/usage

--- src/main/java/com/lingk/aws/session/api/AWSSessionTokenUsage.java
+++ src/main/java/com/lingk/aws/session/api/AWSSessionTokenUsage.java
@@ -2,15 +2,12 @@ package com.lingk.aws.session.api;
 
 import java.text.MessageFormat;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.fission.Context;
@@ -20,33 +17,20 @@ import io.fission.Function;
 public class AWSSessionTokenUsage implements Function {
 	ObjectMapper mapper = new ObjectMapper();
 
-	@SuppressWarnings("unchecked")
 	public ResponseEntity call(RequestEntity req, Context context) {
 		StringBuffer sb = new StringBuffer();
 		try {
 			HashMap data = (HashMap) req.getBody();
-			sb.append("#linux");
+			sb.append("#####linux\n\n");
 			for (Object key : data.keySet()) {
 				sb.append(MessageFormat.format("export {0}={1}\n", key, data.get(key)));
 			}
-		} catch (Exception e) {
-			sb.append(e.getMessage());
-		}
-		HttpHeaders httpHeaders = new HttpHeaders();
-		return new ResponseEntity<StringBuffer>(sb, httpHeaders, HttpStatus.OK);
-	}
 
-	@SuppressWarnings("unchecked")
-	public ResponseEntity call2(RequestEntity req, Context context) {
-		StringBuffer sb = new StringBuffer();
-		try {
-			JsonNode body = mapper.readTree(req.getBody().toString());
-			Iterator<Map.Entry<String, JsonNode>> jsonFields = body.fields();
-			sb.append("#linux");
-			while (jsonFields.hasNext()) {
-				Map.Entry<String, JsonNode> next = jsonFields.next();
-				sb.append(MessageFormat.format("export {}={}\n", next.getKey(), next.getValue().asText()));
+			sb.append("\n\n#####windows\n\n");
+			for (Object key : data.keySet()) {
+				sb.append(MessageFormat.format("set {0}={1}\n", key, data.get(key)));
 			}
+
 		} catch (Exception e) {
 			sb.append(e.getMessage());
 		}
