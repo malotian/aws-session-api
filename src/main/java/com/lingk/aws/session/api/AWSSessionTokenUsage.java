0f980b293f4ba328eb629d042c8d35b5edb481a1
Harjinder Dhanjal <malotian@gmail.com>
2019-11-11 19:42:37 -0500

    help/usage

--- src/main/java/com/lingk/aws/session/api/AWSSessionTokenUsage.java
+++ src/main/java/com/lingk/aws/session/api/AWSSessionTokenUsage.java
@@ -1,27 +1,41 @@
 package com.lingk.aws.session.api;
 
+import java.io.IOException;
 import java.text.MessageFormat;
-import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.fission.Context;
 import io.fission.Function;
 
 @SuppressWarnings("rawtypes")
 public class AWSSessionTokenUsage implements Function {
+	ObjectMapper mapper = new ObjectMapper();
+
 	@SuppressWarnings("unchecked")
 	public ResponseEntity call(RequestEntity req, Context context) {
-		HashMap data = (HashMap) req.getBody();
-		StringBuffer sb = new StringBuffer();
-		sb.append("#linux");
-		for (Object key : data.keySet()) {
-			sb.append(MessageFormat.format("export {}={}\n", key, data.get(key)));
-		}
 
+		StringBuffer sb = new StringBuffer();
+		try {
+			JsonNode body = mapper.readTree(req.getBody().toString());
+			Iterator<Map.Entry<String, JsonNode>> jsonFields = body.fields();
+			sb.append("#linux");
+			while (jsonFields.hasNext()) {
+				Map.Entry<String, JsonNode> next = jsonFields.next();
+				sb.append(MessageFormat.format("export {}={}\n", next.getKey(), next.getValue().asText()));
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		HttpHeaders httpHeaders = new HttpHeaders();
 		httpHeaders.setContentType(org.springframework.http.MediaType.TEXT_PLAIN);
 		return new ResponseEntity(sb.toString(), httpHeaders, HttpStatus.OK);
