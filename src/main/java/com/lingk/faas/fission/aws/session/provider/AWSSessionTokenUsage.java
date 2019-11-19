6ee782dba7e32e92d775cec618a613e2e4704ede
Harjinder Dhanjal <malotian@gmail.com>
2019-11-19 21:37:08 +0000

    cleanup

--- src/main/java/com/lingk/faas/fission/aws/session/provider/AWSSessionTokenUsage.java
+++ src/main/java/com/lingk/faas/fission/aws/session/provider/AWSSessionTokenUsage.java
@@ -1,11 +1,7 @@
 package com.lingk.faas.fission.aws.session.provider;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.text.MessageFormat;
 
 import org.joda.time.DateTime;
@@ -40,7 +36,7 @@ public class AWSSessionTokenUsage implements Function {
 		try {
 			final MultiValueMap<String, String> parameters = UriComponentsBuilder.fromUri(req.getUrl()).build().getQueryParams();
 			final Jwt jwt = JwtHelper.decode(parameters.getFirst("jwt"));
-			final JsonNode claims = mapper.readTree(jwt.getClaims());
+			final JsonNode claims = AWSSessionTokenUsage.mapper.readTree(jwt.getClaims());
 			final DateTime dt = new DateTime(claims.get("Expiration").asText());
 			sb.append("# copy and execute following commands on terminal\n");
 			sb.append(MessageFormat.format("`aws configure set aws_access_key_id {0} --profile lingk-fission`\n\n", claims.get("AccessKeyId")));
@@ -48,7 +44,7 @@ public class AWSSessionTokenUsage implements Function {
 			sb.append(MessageFormat.format("`aws configure set aws_session_token {0} --profile lingk-fission`\n\n", claims.get("SessionToken")));
 			sb.append(MessageFormat.format("`aws eks --region {0} update-kubeconfig --name fission-{0} --profile lingk-fission`\n\n\n", "us-east-1"));
 			sb.append(MessageFormat.format("#### note: with above fission access will be valid till: {0}", dt.toString(DateTimeFormat.fullDateTime())));
-			StringBuilder template = new StringBuilder();
+			final StringBuilder template = new StringBuilder();
 			try (BufferedReader br = new BufferedReader(new InputStreamReader(AWSSessionTokenUsage.class.getClassLoader().getResourceAsStream("template.html")))) {
 				String line;
 				while ((line = br.readLine()) != null) {
@@ -56,8 +52,8 @@ public class AWSSessionTokenUsage implements Function {
 				}
 			}
 
-			String response = template.toString().replaceAll("<!--MARKDOWN HERE-->", sb.toString());
-			return new ResponseEntity<String>(response, httpHeaders, HttpStatus.OK);
+			final String response = template.toString().replaceAll("<!--MARKDOWN HERE-->", sb.toString());
+			return new ResponseEntity<>(response, httpHeaders, HttpStatus.OK);
 
 		} catch (final Exception e) {
 			sb.append(e.getMessage());
