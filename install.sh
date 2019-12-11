4208a7aab67f02a3d5537f86e70ccff46bd5556b
Harjinder Dhanjal <malotian@gmail.com>
2019-12-11 20:15:41 +0000

    fixed install script

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@ set -x #echo on
 name=`basename $(pwd)`
 zip -r ${name}.zip src pom.xml build.sh
 fission env create --name java --image fission/jvm-env --builder fission/jvm-builder --keeparchive --version 2
-pkgname=`fission pkg create --sourcearchive ${name}.zip --env java | cut -f2 -d "'"`
-echo $pkgname
+fission pkg delete --name ${name} -f
+fission pkg create --name ${name} --sourcearchive ${name}.zip --env java
 fission fn delete --name ${name}-usage
-fission fn create --name ${name}-usage --pkg $pkgname --fntimeout 180 --entrypoint com.lingk.faas.fission.aws.session.provider.AWSSessionTokenUsage 
-fission route create --url /${name}-usage --function ${name}-usage --createingress
+fission fn create --name ${name}-usage --pkg ${name} --fntimeout 180 --entrypoint com.lingk.faas.fission.aws.session.provider.AWSSessionTokenUsage
+fission route delete --name ${name}-usage
+fission route create --name ${name}-usage --url /${name}-usage --function ${name}-usage --createingress
